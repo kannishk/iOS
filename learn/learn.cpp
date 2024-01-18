4944484f276a68a25472ecc8fa1ae4e8bc118483
Kanishk <kanishk.chores@gmail.com>
2024-01-18 20:27:43 +0530

    Projects

---
+++ learn/learn.cpp
@@ -0,0 +1,23 @@
+//
+//  learn.cpp
+//  Scrumdinger
+//
+//  Created by Kanishk Tiwari on 25/10/23.
+//
+
+#include <stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cout<<"Hello World"<<endl;
+    
+    
+    
+    return 0;
+}
---
+++ weSplit/weSplit/weSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  weSplitApp.swift
+//  weSplit
+//
+//  Created by Kanishk Tiwari on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct weSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
