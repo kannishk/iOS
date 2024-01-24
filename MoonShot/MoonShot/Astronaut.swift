b26f1a2aea0ef60994798d3313feb045304a4b7f
Kanishk <kanishk.chores@gmail.com>
2024-01-24 14:58:18 +0530

    Project added moonshot

---
+++ MoonShot/MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by Kanishk Tiwari on 24/01/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ MoonShot/MoonShot/MoonShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotApp.swift
+//  MoonShot
+//
+//  Created by Kanishk Tiwari on 23/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
