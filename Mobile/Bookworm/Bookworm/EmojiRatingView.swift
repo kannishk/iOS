c6d3f9f0120016626afac282ac4040f4f5ba8403
Kanishk <kanishk.chores@gmail.com>
2024-02-10 02:05:52 +0530

    Last project & Segmentation

---
+++ Mobile/Bookworm/Bookworm/EmojiRatingView.swift
@@ -0,0 +1,30 @@
+//
+//  EmojiRatingView.swift
+//  Bookworm
+//
+//  Created by Kanishk Tiwari on 27/01/24.
+//
+
+import SwiftUI
+
+struct EmojiRatingView: View {
+    let rating:Int
+    
+    var body: some View {
+        switch rating{
+        case 1:
+            Text("😀")
+        case 2:
+            Text("😃")
+        case 3:
+            Text("😄")
+        case 4:
+            Text("🤣")
+        default:
+            Text("🙃")
+        }
+    }
+}
+
+#Preview {
+    EmojiRatingView(rating: 3)}
